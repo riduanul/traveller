9d581e3f91f463c29e2b201a22715ecee182742e
riduanul <67514628+riduanul@users.noreply.github.com>
2020-09-16 16:04:35 +0600

    traveller starting code

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../resorces/Logo.png';
+
+const Header = () => {
+    return (
+        <div>
+            <img src={log} alt=""/>
+            <input type="text"/>
+        </div>
+    );
+};
+
+export default Header;
